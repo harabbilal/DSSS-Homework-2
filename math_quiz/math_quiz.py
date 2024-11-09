a31da8f0fe1c8721c7e29662c2d513777f2cb992
Muhammad Bilal <80957963+harabbilal@users.noreply.github.com>
2024-11-09 16:40:24 +0100

    Update math_quiz.py

--- math_quiz/math_quiz.py
+++ math_quiz/math_quiz.py
@@ -3,18 +3,24 @@
 import random
 
 
-def number_limit(min, max): # Function definition for random intialization of numbers
+def number_intialization(min, max): # Function definition for random intialization of numbers
     """
     Random integer.
     """
     return random.randint(min, max)
 
 
-def math_operation(): # Function definition for random choice of math operator
+def operator_selection(): # Function definition for random choice of math operator
+    """
+    Random operator selection.
+    """
     return random.choice(['+', '-', '*'])
 
 
-def calculation(n1, n2, o): # Function definition for question intialization 
+def questions_intialization(n1, n2, o): # Function definition for question intialization 
+    """
+    Quiz Question Intialization.
+    """
     p = f"{n1} {o} {n2}"
     if o == '+': a = n1 + n2 # these lines are intializing the questions based on operator
     elif o == '-': a = n1 - n2
@@ -22,19 +28,26 @@ def calculation(n1, n2, o): # Function definition for question intialization
     return p, a
 
 def math_quiz(): 
+    """
+    This is the main quiz function in s represent the current turn and t_q represent the total turns allowed to every user.
+    """
     s = 0
-    t_q = 3 # intiaizing the number of turns
+    t_q = 5 # intiaizing the number of turns for user
 
     print("Welcome to the Math Quiz Game!")
     print("You will be presented with math problems, and you need to provide the correct answers.")
 
     for _ in range(t_q):
-        n1 = number_limit(1, 10); n2 = number_limit(1, 5); o = math_operation()
+        n1 = number_intialization(1, 10); n2 = number_intialization(1, 5); o = operator_selection()
 
-        PROBLEM, ANSWER = calculation(n1, n2, o)
+        PROBLEM, ANSWER = questions_intialization(n1, n2, o)
         print(f"\nQuestion: {PROBLEM}")
-        useranswer = input("Your answer: ") # Getting answer from the user
-        useranswer = int(useranswer)
+        try:
+            useranswer = input("Your answer: ") # Getting answer from the user
+            useranswer = int(useranswer)
+        
+        except InvalidInput:
+            print("Only numeric input allowed")
 
         if useranswer == ANSWER: # if else statement to check the answer
             print("Correct! You earned a point.")
